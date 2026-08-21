97973db2dbb52b34579640f448d98c368040f62b
Vladislav Zyshchuk <vlad.zyshchuk@intexsoft.by>
2026-08-21 16:33:27 +0300

    chapter 16 (end)

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,22 +1,27 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination'
+import Search from '@/app/ui/search'
+import Table from '@/app/ui/invoices/table'
+import { CreateInvoice } from '@/app/ui/invoices/buttons'
+import { lusitana } from '@/app/ui/fonts'
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
+import { Suspense } from 'react'
+import { fetchInvoicesPages } from '@/app/lib/data'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page(props: {
   searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
+    query?: string
+    page?: string
+  }>
 }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchInvoicesPages(query);
+  const searchParams = await props.searchParams
+  const query = searchParams?.query || ''
+  const currentPage = Number(searchParams?.page) || 1
+  const totalPages = await fetchInvoicesPages(query)
 
   return (
     <div className="w-full">
@@ -34,5 +39,5 @@ export default async function Page(props: {
         <Pagination totalPages={totalPages} />
       </div>
     </div>
-  );
+  )
 }
